ee2d278141a7c1e5b83f4372b844e833aa6837ac
Maruf Hasan Shihab <marufhasanshihab2@gmail.com>
2024-08-10 15:26:17 +0600

    compleate full project

--- src/Components/Layouts/MainLayoute/MainLayoute.jsx
+++ src/Components/Layouts/MainLayoute/MainLayoute.jsx
@@ -1,8 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../../Navbar/Navbar";
+import { useEffect } from "react";
 
 
 const MainLayoute = () => {
+    const location = useLocation()
+    useEffect(()=>{
+        if(location.pathname === "/"){
+            document.title =`phone-home`
+        }else{
+            document.title = `phone-${location.pathname.replace('/','')}`
+        }
+        if(location.state){
+            document.title = `phone-${location.state}`
+        }
+    },[location.pathname])
     return (
         <div className="px-14">
             <Navbar></Navbar>
--- src/Components/Pages/Favorites/Favorites.jsx
+++ src/Components/Pages/Favorites/Favorites.jsx
@@ -6,10 +6,13 @@ const Favorites = () => {
   const [favorites, setFavorites] = useState([]);
   const [notFound, setNotFound] = useState("");
   const [isShow, setIsShow] = useState(false)
+  const [totalPrice, setTotalPrice] = useState(0);
   useEffect(() => {
-    const favoritesItem = JSON.parse(localStorage.getItem("favorites"));
-    if (favoritesItem) {
-      setFavorites(favoritesItem)
+    const favoritesItems = JSON.parse(localStorage.getItem("favorites"));
+    if (favoritesItems) {
+      setFavorites(favoritesItems)
+      const total = favoritesItems.reduce((prevValue, currItem)=> prevValue + currItem.price ,0)
+      setTotalPrice(total)
     } else {
       setNotFound("No Item");
     }
@@ -23,7 +26,8 @@ const Favorites = () => {
     <div>
       <div>
         {favorites.length > 0 && (
-         <button className="flex mt-6 mx-auto" onClick={handleRemoveAllFavorites}> <a
+        <div>
+           <button className="flex mt-6 mx-auto" onClick={handleRemoveAllFavorites}> <a
          className="relative inline-flex items-center justify-center inline-block p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 rounded-lg shadow-2xl group f"
        >
          <span className="absolute top-0 left-0 w-40 h-40 -mt-10 -ml-3 transition-all duration-700 bg-red-500 rounded-full blur-md ease"></span>
@@ -33,6 +37,8 @@ const Favorites = () => {
          </span>
          <span className="relative text-white">Delete All Favorites</span>
        </a></button>
+       <h1 className="my-4 text-center font-semibold">Total Price : {totalPrice}</h1>
+        </div>
         )}
       </div>
       {notFound ? (
